800c84daafd93a3ae08a9c58e961b14f63676591
James Dawson <james@readsource.co.uk>
2020-04-22 08:43:33 +0000

    Add/fix the remaining linked templates Allow a project to provide additional linked templates

--- _deployContainer/endjin.deployment/functions/azure/DeployArmTemplate.ps1
+++ _deployContainer/endjin.deployment/functions/azure/DeployArmTemplate.ps1
@@ -9,8 +9,13 @@ function DeployArmTemplate
         [Hashtable]$DeploymentContext
     )
 
-    $ArmTemplatePath = Join-Path $ArtifactsFolderPath $TemplateFileName -Resolve
-    $OptionalParameters = @{marainPrefix=$DeploymentContext.Prefix;environmentSuffix=$DeploymentContext.EnvironmentSuffix}
+    $ArtifactsFolderPaths = @(
+        (Join-Path $PSScriptRoot '../../arm-templates' -Resolve)
+        $ArtifactsFolderPath
+    )
+    
+    $OptionalParameters = @{}
+    # $OptionalParameters = @{marainPrefix=$DeploymentContext.Prefix;environmentSuffix=$DeploymentContext.EnvironmentSuffix}
     $ResourceGroupName = $DeploymentContext.DefaultResourceGroupName
 
     $ArtifactsLocationName = '_artifactsLocation'
@@ -33,17 +38,21 @@ function DeployArmTemplate
     # Copy files from the local storage staging location to the storage account container
     New-AzStorageContainer -Name $StorageContainerName -Context $StorageAccount.Context -ErrorAction SilentlyContinue *>&1
 
-    $ArtifactFilePaths = Get-ChildItem $ArtifactsFolderPath -Recurse -File | ForEach-Object -Process {$_.FullName}
-    foreach ($SourcePath in $ArtifactFilePaths) {
-        Set-AzStorageBlobContent `
-            -File $SourcePath `
-            -Blob $SourcePath.Substring($ArtifactsFolderPath.length + 1) `
-            -Container $StorageContainerName `
-            -Context $StorageAccount.Context `
-            -Force | Out-Null
+    foreach ($ArtifactsFolderPath in $ArtifactsFolderPaths) {
+        $ArtifactFilePaths = Get-ChildItem $ArtifactsFolderPath -Recurse -File | ForEach-Object -Process {$_.FullName}
+        
+        foreach ($SourcePath in $ArtifactFilePaths) {
+            Set-AzStorageBlobContent `
+                -File $SourcePath `
+                -Blob $SourcePath.Substring($ArtifactsFolderPath.length + 1) `
+                -Container $StorageContainerName `
+                -Context $StorageAccount.Context `
+                -Force | Out-Null
+        }
     }
 
     $OptionalParameters[$ArtifactsLocationName] = $StorageAccount.Context.BlobEndPoint + $StorageContainerName
+    # $TemplateParameters[$ArtifactsLocationName] = ($StorageAccount.Context.BlobEndPoint + $StorageContainerName).Trim()
     # Generate a 4 hour SAS token for the artifacts location if one was not provided in the parameters file
     $StagingSasToken = New-AzStorageContainerSASToken `
         -Name $StorageContainerName `
@@ -51,16 +60,19 @@ function DeployArmTemplate
         -Permission r `
         -ExpiryTime (Get-Date).AddHours(4)
     $OptionalParameters[$ArtifactsLocationSasTokenName] = ConvertTo-SecureString -AsPlainText -Force $StagingSasToken
+    # $TemplateParameters[$ArtifactsLocationSasTokenName] = (ConvertTo-SecureString -AsPlainText -Force $StagingSasToken.Trim())
 
     # Create the resource group only when it doesn't already exist
     if ((Get-AzResourceGroup -Name $ResourceGroupName -Location $DeploymentContext.AzureLocation -Verbose -ErrorAction SilentlyContinue) -eq $null) {
         New-AzResourceGroup -Name $ResourceGroupName -Location $DeploymentContext.AzureLocation -Verbose -Force -ErrorAction Stop
     }
 
+    $ErrorActionPreference = 'Continue'
+    $armDeployName = "{0}-{1}" -f (Get-ChildItem $TemplateFileName).BaseName, ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')
     $DeploymentResult = New-AzResourceGroupDeployment `
-        -Name ((Get-ChildItem $ArmTemplatePath).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+        -Name $armDeployName `
         -ResourceGroupName $ResourceGroupName `
-        -TemplateFile $ArmTemplatePath `
+        -TemplateFile $TemplateFileName `
         @OptionalParameters `
         @TemplateParameters `
         -Force -Verbose `

1fc74f620ec581c56613521b47596c6eb43b67d5
James Dawson <james.dawson@endjin.com>
2020-05-20 21:23:01 +0100

    teardown script tweaks

--- Solutions/Marain.Instance.Deployment/quick-teardown.ps1
+++ Solutions/Marain.Instance.Deployment/quick-teardown.ps1
@@ -4,11 +4,12 @@ param
     [string] [Parameter(Mandatory=$true)] $EnvironmentSuffix,
     [string] $Prefix = "mar"
 )
-$ErrorActionPreference = 'Continue'
+$ErrorActionPreference = 'Stop'
 
-Write-Warning "About delete ALL Azure resources: Prefix=$Prefix; Environment=$EnvironmentSuffix"
+Write-Warning "About to delete ALL Marain resources:`n`tSubscription: $((Get-AzContext).Name)`n`tPrefix: $Prefix`n`tEnvironment: $EnvironmentSuffix"
 Read-Host "Press Key To Confirm"
 
+$ErrorActionPreference = 'Continue'
 $groups = @(
     "tenancy"
     "operations"
@@ -19,9 +20,13 @@ $jobs = @()
 foreach ($g in $groups) {
     $group = "{0}.{1}.{2}" -f $Prefix, $g, $EnvironmentSuffix
     Write-Host "Deleting resource group: $group ..."
-    $jobs += Start-Job -Name $g -ScriptBlock { Remove-AzResourceGroup -Name $group -Force }
+    $jobs += Start-Job -Name $g -ScriptBlock { Remove-AzResourceGroup -Name $using:group -Force }
 }
-$jobs | % { Write-Host "Waiting for job: $($_.Name)"; Wait-Job $_ | Out-Null }
+$jobs | % {
+    Write-Host "Waiting for job: $($_.Name)"
+    Wait-Job $_ | Out-Null
+    Receive-Job $_
+}
 Write-Host "All jobs completed`n"
 
 $aadApps = @(

136fe0819cb7df1bb9eb4d9fbe6bd77b41729e2b
James Dawson <james.dawson@endjin.com>
2020-11-30 17:53:29 +0000

    Make idempotence check Strict-Mode compliant

--- Solutions/Marain.Instance.Deployment/Deploy-MarainInstanceInfrastructure.ps1
+++ Solutions/Marain.Instance.Deployment/Deploy-MarainInstanceInfrastructure.ps1
@@ -597,8 +597,8 @@ class MarainServiceDeploymentContext {
         # as part of a future major update
         $getUri = "https://graph.windows.net/$($this.InstanceContext.TenantId)/servicePrincipals/$ClientIdentityServicePrincipalId/appRoleAssignedTo?api-version=1.6"
         $response = Invoke-AzCliRestCommand -Uri $getUri
-        $existing = $response.value
-        if ($existing.id -contains $TargetAppRoleId) {
+        $existingRoleAssignments = $response.value | Where-Object { $_ } | ForEach-Object { $_.id }
+        if ($existingRoleAssignments -contains $TargetAppRoleId) {
             Write-Host "Already assigned: role $TargetAppRoleId for app $TargetAppId sp: $TargetAccessControlServicePrincipalId to client $ClientAppNameWithSuffix (sp: $ClientIdentityServicePrincipalId)"
         }
         else {
